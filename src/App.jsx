08503e26f481885cd8d88ee3d4465b07ee1d027d
ariesdimasy <ariesdimasy@gmail.com>
2023-11-16 22:10:28 +0700

    git init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavbarSection from './sections/NavbarSection'
+import HeroSection from './sections/HeroSection'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <NavbarSection></NavbarSection>
+      <HeroSection></HeroSection>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,19 @@
+export default function HeroSection() { 
+    return (<section className="hero-section">
+        <div className="container">
+            <div className="left-side"></div>
+            <div className="right-side">
+                <div className="text">
+                    <div className="hello">
+                        <div className="box">
+                            HELLO !
+                        </div>
+                    </div>
+                    <div className="introduction">
+                        Iam Aries Dimas a Web Development Lecturer
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>)
+}
